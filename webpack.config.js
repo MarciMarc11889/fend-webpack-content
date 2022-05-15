fffcc0fbb1d412be8508a97365e76f2b98fa3c9d
Marc pr <marc.rene@web.de>
2022-05-15 22:16:35 +0200

    included file loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,26 +6,20 @@ module.exports = {
     module: {
         rules: [
                 {
-            test: '/\.js$/',
-            exclude: /node_modules/,
-            loader: "babel-loader"
-                }
+                  test: '/\.js$/',
+                  exclude: /node_modules/,
+                  loader: "babel-loader"
+                },
+                {
+                  test: /\.(png|jpe?g|gif)$/i,
+                  use: [
+                    {
+                      loader: 'file-loader',
+                    },
+                  ],
+                },
                 
         ]
         
 }
-}
-module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.(png|jpe?g|gif)$/i,
-          use: [
-            {
-              loader: 'file-loader',
-            },
-          ],
-        },
-      ],
-    },
   };
